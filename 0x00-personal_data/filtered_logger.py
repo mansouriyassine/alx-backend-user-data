08d92787308aba248bc2d600efe5ec7ee49884a0
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-16 14:35:40 +0100

    0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,13 +5,13 @@ import re
 def filter_datum(fields, redaction, message, separator):
     """
     Obfuscates specified fields in a log message using regex.
-    
+
     Args:
     fields (list): A list of strings representing all fields to obfuscate.
     redaction (str): The string by which the field will be obfuscated.
     message (str): The log line.
     separator (str): The character separating all fields in the log line.
-    
+
     Returns:
     str: The obfuscated log message.
     """

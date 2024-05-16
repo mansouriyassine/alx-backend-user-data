59c7d6d1d3b486d27dcd6a6e6a11eb98e6c7d6ff
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-16 14:52:31 +0100

    0

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""
+Module for password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a given password using bcrypt.
+    
+    Args:
+    password (str): The plaintext password to hash.
+    
+    Returns:
+    bytes: The hashed password.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates if a given password matches the hashed password.
+    
+    Args:
+    hashed_password (bytes): The hashed password to compare against.
+    password (str): The plaintext password to check.
+    
+    Returns:
+    bool: True if the password matches the hashed password, False otherwise.
+    """
+    decoded_hashed_password = hashed_password.decode()
+    return bcrypt.checkpw(password.encode(), decoded_hashed_password.encode())

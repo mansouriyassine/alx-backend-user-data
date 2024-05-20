f6a966db88c48b992f7b36456fc2dfd8ff2fb142
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-20 17:14:28 +0100

    4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,12 +11,19 @@ class Auth:
         """
         Determines if authentication is required
         """
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        if path is None:
             return True
+        
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        
         if path[-1] != '/':
             path += '/'
-        if path in excluded_paths:
-            return False
+        
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+        
         return True
 
     def authorization_header(self, request=None) -> str:

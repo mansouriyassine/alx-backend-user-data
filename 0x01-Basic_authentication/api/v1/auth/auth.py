b9efcce90a51d800a997697b4b7dba7963f36af6
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-20 17:09:57 +0100

    3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,16 +11,24 @@ class Auth:
         """
         Determines if authentication is required
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
-        Returns the authorization header from the request
+        Returns the value of the authorization header from the request
         """
-        return None
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        Returns the current user
+        Returns None - request will be the Flask request object
         """
         return None

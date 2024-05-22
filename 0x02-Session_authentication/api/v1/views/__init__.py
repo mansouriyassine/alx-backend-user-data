f3c754b4dd5b5236ea5ea6b43e2fff908c6e4742
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-22 21:16:22 +0100

    6

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -7,6 +7,5 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
-from api.v1.views.session_auth import auth_session_login  # Import the new function here
 
 User.load_from_file()

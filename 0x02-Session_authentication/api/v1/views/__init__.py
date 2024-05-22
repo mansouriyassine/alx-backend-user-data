ea9f0bcea0fbb215bd350fb42ebcce69f418059c
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-22 21:11:50 +0100

    6

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -10,6 +10,3 @@ from api.v1.views.users import *
 from api.v1.views.session_auth import auth_session_login  # Import the new function here
 
 User.load_from_file()
-
-# Register the new route with the app_views blueprint
-app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)(auth_session_login)

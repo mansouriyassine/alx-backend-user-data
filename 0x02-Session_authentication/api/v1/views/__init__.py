da22da8a7a060aac9dcfd0e4b5e653346bd4082e
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-22 21:06:14 +0100

    7

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -7,5 +7,9 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
+from api.v1.views.session_auth import auth_session_login  # Import the new function here
 
 User.load_from_file()
+
+# Register the new route with the app_views blueprint
+app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)(auth_session_login)
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module for the API
+"""
+
+from api.v1.auth.session_auth import SessionAuth
+from flask import jsonify, request, make_response, abort
+from models.user import User
+from os import getenv
+
+sa = SessionAuth()
+
+@sa.exempt
+def auth_session_login() -> str:
+    """
+    Function to login using session auth
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email:
+        abort(jsonify({"error": "email missing"}), 400)
+    if not password:
+        abort(jsonify({"error": "password missing"}), 400)
+    try:
+        user = User.search(email)
+    except Exception as e:
+        abort(jsonify({"error": "no user found for this email"}), 404)
+    if not user.is_valid_password(password):
+        abort(jsonify({"error": "wrong password"}), 401)
+    auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    session_name = getenv('SESSION_NAME', '_my_session_id')
+    response.set_cookie(session_name, auth.user_id_by_session_id)
+    return response

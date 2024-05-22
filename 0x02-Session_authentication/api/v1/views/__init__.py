cbcc597d49e26b4792f718a5c469e6981dbfa66d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-22 21:25:40 +0100

    7

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
-"""
-DocDocDocDocDocDoc
+""" DocDocDocDocDocDoc
 """
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import index
-from api.v1.views.users import show_users, show_user, create_user, update_user, destroy_user
-from api.v1.views.session_auth import auth_session_login
-
-from models.user import User
+from api.v1.views.index import *
+from api.v1.views.users import *
 
 User.load_from_file()
-
-app_views.add_url_rule('/', 'index', index)
-app_views.add_url_rule('/users', 'show_users', show_users)
-app_views.add_url_rule('/users/<user_id>', 'show_user', show_user)
-app_views.add_url_rule('/users', 'create_user', create_user, methods=['POST'])
-app_views.add_url_rule('/users/<user_id>', 'update_user', update_user, methods=['PUT'])
-app_views.add_url_rule('/users/<user_id>', 'destroy_user', destroy_user, methods=['DELETE'])
-
-app_views.add_url_rule('/auth_session/login', 'auth_session_login', auth_session_login, methods=['POST'])

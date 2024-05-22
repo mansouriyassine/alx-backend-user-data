56ba3aecbb6e1ece5c86be2beaf1fa2e5b11c50d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-22 20:40:20 +0100

    3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -41,5 +41,5 @@ class SessionAuth(Auth):
         """
         if session_id is None or not isinstance(session_id, str):
             return None
-        
+
         return self.user_id_by_session_id.get(session_id)

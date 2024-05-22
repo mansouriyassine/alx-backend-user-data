80ad598bb590f0b025fe3c94eac18b46a8673bcd
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-22 20:36:18 +0100

    2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,13 +19,13 @@ class SessionAuth(Auth):
         Creates a Session ID for a user_id.
         Args:
             user_id (str): The user ID to create a session for.
-        
+
         Returns:
             str: The session ID if successful, None otherwise.
         """
         if user_id is None or not isinstance(user_id, str):
             return None
-        
+
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id

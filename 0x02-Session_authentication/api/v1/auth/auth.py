f9afdd3fc5e34a517dbe6b912f100ea955e3a110
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-22 20:45:48 +0100

    4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -95,6 +95,6 @@ class Auth:
         """
         if request is None:
             return None
-        
+
         session_name = os.getenv('SESSION_NAME', '_my_session_id')
         return request.cookies.get(session_name)

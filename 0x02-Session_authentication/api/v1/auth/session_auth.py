4bc5bb6517c14426fd42687dd3bbe469f3c6eb1e
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-22 20:23:33 +0100

    1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module for the API
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class that inherits from Auth
+    This class will be used for session-based authentication.
+    """
+    pass

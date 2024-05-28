23184cf473a2664c6fc2883b9b45b7291b749c77
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-28 14:37:32 +0100

    5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,7 +5,7 @@ Authentication module
 import bcrypt
 from db import DB
 from user import User
-
+from sqlalchemy.orm.exc import NoResultFound
 
 class Auth:
     """Auth class to interact with the authentication database.
@@ -40,16 +40,19 @@ class Auth:
         Raises:
             ValueError: If a user with the given email already exists.
         """
+        # Check if the user already exists
         try:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
-        except ValueError:
-            pass
+        except NoResultFound:
+            pass  # User not found, continue with registration
         except Exception as e:
-            raise e
+            raise e  # Propagate any unexpected errors
 
+        # Hash the password
         hashed_password = self._hash_password(password)
 
+        # Add the user to the database
         user = self._db.add_user(email, hashed_password)
 
         return user

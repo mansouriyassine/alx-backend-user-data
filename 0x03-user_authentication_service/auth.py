dabb88573450802679cd65d94b88f1e8f33960f0
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-28 15:23:20 +0100

    5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,25 +7,25 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
+def _hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The salted hash of the password.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """Hashes a password using bcrypt.
-
-        Args:
-            password (str): The password to hash.
-
-        Returns:
-            bytes: The salted hash of the password.
-        """
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed_password
 
     def register_user(self, email: str, password: str) -> User:
         """Registers a new user.
@@ -50,7 +50,7 @@ class Auth:
             raise e  # Propagate any unexpected errors
 
         # Hash the password
-        hashed_password = self._hash_password(password)
+        hashed_password = _hash_password(password)
 
         # Add the user to the database
         user = self._db.add_user(email, hashed_password)
@@ -59,4 +59,4 @@ class Auth:
 
 # For testing the function
 if __name__ == "__main__":
-    print(Auth()._hash_password("Hello Holberton"))
+    print(_hash_password("Hello Holberton"))

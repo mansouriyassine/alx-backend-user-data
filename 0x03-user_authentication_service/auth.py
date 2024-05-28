3a64b22e9aa253f4836db43bdc434159cf28351d
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-28 14:28:09 +0100

    4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+"""
+Authentication module
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The salted hash of the password.
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+if __name__ == "__main__":
+    print(_hash_password("Hello Holberton"))

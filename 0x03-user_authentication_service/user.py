1fe4c0493fc78039725fbf10cff43f9c2b90111e
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-28 13:52:00 +0100

    0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,1 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+User model definition
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    """SQLAlchemy User model for the users table."""
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+if __name__ == "__main__":
+    engine = create_engine('sqlite:///:memory:', echo=True)
+    Base.metadata.create_all(engine)

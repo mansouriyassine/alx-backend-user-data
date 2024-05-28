5eb5843901cbddb2fa42c407558dad5453b89d43
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-28 15:28:22 +0100

    6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+"""
+Flask application.
+"""
+
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    """
+    Handle GET requests on the root endpoint.
+
+    Returns:
+        dict: A JSON response with a welcome message.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

ffcf60126fc7f183be3d85dd62439b6d6d91b2f3
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-29 15:34:12 +0100

    19

--- 0x03-user_authentication_service/app copy.py
+++ 0x03-user_authentication_service/app copy.py
@@ -16,7 +16,6 @@ Usage:
         - DELETE http://localhost:5000/sessions
         - GET http://localhost:5000/profile
         - POST http://localhost:5000/reset_password
-        - PUT http://localhost:5000/reset_password
 
     Payload:
         - For user registration (POST /users):
@@ -29,20 +28,16 @@ Usage:
             Cookie: 'session_id'
         - For reset password token (POST /reset_password):
             Form data: 'email'
-        - For updating password (PUT /reset_password):
-            Form data: 'email', 'reset_token', 'new_password'
 
     Response:
         - For user registration:
             - 200 OK: User created successfully
-                {"email": "<registered email>", 
-                 "message": "user created"}
+                {"email": "<registered email>", "message": "user created"}
             - 400 BAD REQUEST: Email already registered
                 {"message": "email already registered"}
         - For user login:
             - 200 OK: User logged in successfully
-                {"email": "<user email>", 
-                 "message": "logged in"}
+                {"email": "<user email>", "message": "logged in"}
             - 401 UNAUTHORIZED: Incorrect login information
         - For user logout:
             - 302 Found: User logged out and redirected to GET /
@@ -53,14 +48,8 @@ Usage:
             - 403 FORBIDDEN: Invalid session ID or user not found
         - For reset password token:
             - 200 OK: Reset password token generated successfully
-                {"email": "<user email>", 
-                 "reset_token": "<reset token>"}
+                {"email": "<user email>", "reset_token": "<reset token>"}
             - 403 FORBIDDEN: Email not registered
-        - For updating password:
-            - 200 OK: Password updated successfully
-                {"email": "<user email>", 
-                 "message": "Password updated"}
-            - 403 FORBIDDEN: Invalid reset token
 """
 from flask import Flask, jsonify, request, abort, make_response, \
     redirect, url_for
@@ -93,8 +82,7 @@ def register_user():
         email = request.form["email"]
         password = request.form["password"]
         user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, 
-                        "message": "user created"}), 200
+        return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError as e:
         return jsonify({"message": str(e)}), 400
 
@@ -141,8 +129,7 @@ def profile():
     Handle GET requests to retrieve a user's profile.
 
     Returns:
-        dict: A JSON response with the user's email or a 403 
-              status code.
+        dict: A JSON response with the user's email or a 403 status code.
     """
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
@@ -158,34 +145,12 @@ def get_reset_password_token():
     Handle POST requests to get a reset password token.
 
     Returns:
-        dict: A JSON response with the reset token or a 403 
-              status code.
+        dict: A JSON response with the reset token or a 403 status code.
     """
     email = request.form["email"]
     try:
         reset_token = AUTH.get_reset_password_token(email)
-        return jsonify({"email": email, 
-                        "reset_token": reset_token}), 200
-    except ValueError:
-        abort(403)
-
-
-@app.route("/reset_password", methods=["PUT"])
-def update_password():
-    """
-    Handle PUT requests to update the password.
-
-    Returns:
-        dict: A JSON response with a success message or a 403 
-              status code.
-    """
-    email = request.form["email"]
-    reset_token = request.form["reset_token"]
-    new_password = request.form["new_password"]
-    try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, 
-                        "message": "Password updated"}), 200
+        return jsonify({"email": email, "reset_token": reset_token}), 200
     except ValueError:
         abort(403)
 
